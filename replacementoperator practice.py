57883abb8c0fc52c0d7e50e02564e2d43881e68c
jubinz <116046828+jubinz@users.noreply.github.com>
2022-11-30 23:33:51 +0530

    replacementoperator

---
+++ replacementoperator practice.py
@@ -0,0 +1,7 @@
+name="Mary"
+age=22
+print("Hello {} your age is {}".format(name,age))
+print("Hello {0} your age is {1}".format(name,age))
+print("Hello {1} Your age is {0}".format(name,age))
+print("Hello {name} Your age is {age}".format(name="mary",age=22))
+
